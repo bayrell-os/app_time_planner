64e8a84f1ab0aa8c150377bea3fe6ef8a8114e69
Ildar Bikmamatov <ildar@bayrell.org>
2021-08-08 20:15:36 +0600

    add php backend

---
+++ backend/public/api/index.php
@@ -0,0 +1,9 @@
+<?php
+
+/* Remove api */
+$uri = $_SERVER['REQUEST_URI'];
+$uri = preg_replace("/^\/api/", "", $uri);
+$_SERVER['REQUEST_URI'] = $uri;
+
+$app = require __DIR__.'/../../bootstrap/app.php';
+$app->run();
